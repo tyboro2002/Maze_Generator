e47cd8f6be1adc9a15aed0e6d2026b368c8592ca
Tybo Verslype <tybo.verslype@ugent.be>
2024-06-29 15:20:40 +0200

    chore: added Recursive_division.py comments I forgot in previous commit

--- generation_algoritms/Recursive_division.py
+++ generation_algoritms/Recursive_division.py
@@ -8,6 +8,17 @@ from settings import Structures
 class RecursiveDivisionMazeGenerator(MazeGenerator):
     """
     A class to generate mazes using the Recursive Division Algorithm.
+
+    Mazes can be created with recursive division, an algorithm which works as follows: Begin with the maze's space with
+    no walls. Call this a chamber. Divide the chamber with a randomly positioned wall (or multiple walls) where each wall
+    contains a randomly positioned passage opening within it. Then recursively repeat the process on the subchambers until
+    all chambers are minimum sized. This method results in mazes with long straight walls crossing their space, making
+    it easier to see which areas to avoid.
+
+    For example, in a rectangular maze, build at random points two walls that are perpendicular to each other. These two
+    walls divide the large chamber into four smaller chambers separated by four walls. Choose three of the four walls at
+    random, and open a one cell-wide hole at a random point in each of the three. Continue in this manner recursively,
+    until every chamber has a width of one cell in either of the two directions.
     """
 
     def __init__(self, maze):
--- main.py
+++ main.py
@@ -8,18 +8,6 @@ from generation_algoritms.Wilson import WilsonMazeGenerator
 from settings import sizeWidth, sizeHeight, animations_dir, animate, mazes_dir
 
 
-# TODO Recursive division method
-# Mazes can be created with recursive division, an algorithm which works as follows: Begin with the maze's space with
-# no walls. Call this a chamber. Divide the chamber with a randomly positioned wall (or multiple walls) where each wall
-# contains a randomly positioned passage opening within it. Then recursively repeat the process on the subchambers until
-# all chambers are minimum sized. This method results in mazes with long straight walls crossing their space, making
-# it easier to see which areas to avoid.
-#
-# For example, in a rectangular maze, build at random points two walls that are perpendicular to each other. These two
-# walls divide the large chamber into four smaller chambers separated by four walls. Choose three of the four walls at
-# random, and open a one cell-wide hole at a random point in each of the three. Continue in this manner recursively,
-# until every chamber has a width of one cell in either of the two directions.
-
 # TODO Fractal Tessellation algorithm
 # On each iteration, this algorithm creates a maze twice the size by copying itself 3 times.
 # At the end of each iteration, 3 paths are opened between the 4 smaller mazes.
